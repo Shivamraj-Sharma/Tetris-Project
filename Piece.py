655f3703fd90f6c8bfdb1573b2a260ac140cf240
Shivamraj-Sharma <sharmashivamraj18@gmail.com>
2024-05-27 18:22:27 +0530

    creating a piece entity

---
+++ Piece.py
@@ -0,0 +1,9 @@
+from Shapes import shapes, shape_colors
+
+class Piece(object):  
+    def __init__(self, x, y, shape):
+        self.x = x
+        self.y = y
+        self.shape = shape
+        self.color = shape_colors[shapes.index(shape)]
+        self.rotation = 0
